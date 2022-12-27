b70556ff6958d5150df88c3722e814f14bf337f7
H2101016 <h2101016@dlenc.co.kr>
2022-12-27 17:45:30 +0900

    init

---
+++ src/test/java/kr/co/songeul/gcconverter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.songeul.gcconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
